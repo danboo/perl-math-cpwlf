127d4e5a5e8ccfdaf1d5a35aad0f29be6d61b7a9
U-VistaBoo\danboo <danboo@VistaBoo.(none)>
2009-11-11 09:17:00 -0500

    first commit of skeleton

---
+++ lib/Math/CPWLF.pm
@@ -0,0 +1,110 @@
+package Math::CPWLF;
+
+use warnings;
+use strict;
+
+=head1 NAME
+
+Math::CPWLF - The great new Math::CPWLF!
+
+=head1 VERSION
+
+Version 0.01
+
+=cut
+
+our $VERSION = '0.01';
+
+
+=head1 SYNOPSIS
+
+Quick summary of what the module does.
+
+Perhaps a little code snippet.
+
+    use Math::CPWLF;
+
+    my $foo = Math::CPWLF->new();
+    ...
+
+=head1 EXPORT
+
+A list of functions that can be exported.  You can delete this section
+if you don't export anything, such as for a purely object-oriented module.
+
+=head1 FUNCTIONS
+
+=head2 function1
+
+=cut
+
+sub function1 {
+}
+
+=head2 function2
+
+=cut
+
+sub function2 {
+}
+
+=head1 AUTHOR
+
+Dan Boorstein, C<< <dan at boorstein.net> >>
+
+=head1 BUGS
+
+Please report any bugs or feature requests to C<bug-math-cpwlf at rt.cpan.org>, or through
+the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Math-CPWLF>.  I will be notified, and then you'll
+automatically be notified of progress on your bug as I make changes.
+
+
+
+
+=head1 SUPPORT
+
+You can find documentation for this module with the perldoc command.
+
+    perldoc Math::CPWLF
+
+
+You can also look for information at:
+
+=over 4
+
+=item * RT: CPAN's request tracker
+
+L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=Math-CPWLF>
+
+=item * AnnoCPAN: Annotated CPAN documentation
+
+L<http://annocpan.org/dist/Math-CPWLF>
+
+=item * CPAN Ratings
+
+L<http://cpanratings.perl.org/d/Math-CPWLF>
+
+=item * Search CPAN
+
+L<http://search.cpan.org/dist/Math-CPWLF/>
+
+=back
+
+
+=head1 ACKNOWLEDGEMENTS
+
+
+=head1 COPYRIGHT & LICENSE
+
+Copyright 2009 Dan Boorstein.
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of either: the GNU General Public License as published
+by the Free Software Foundation; or the Artistic License.
+
+See http://dev.perl.org/licenses/ for more information.
+
+
+=cut
+
+1; # End of Math::CPWLF
