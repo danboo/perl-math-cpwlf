a376c5823623b135657e06383a4130f4191a774c
Dan Boorstein <danboo@danboo-desktop.(none)>
2009-12-03 00:25:21 -0500

    - move to version 0.12

--- lib/Math/CPWLF.pm
+++ lib/Math/CPWLF.pm
@@ -21,11 +21,11 @@ Math::CPWLF - interpolation using nested continuous piece-wise linear functions
 
 =head1 VERSION
 
-Version 0.11
+Version 0.12
 
 =cut
 
-our $VERSION = '0.11';
+our $VERSION = '0.12';
 
 =head1 SYNOPSIS
 
